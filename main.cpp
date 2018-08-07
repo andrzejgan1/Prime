9883ecf4399a6a16e8e36dacffb59c6b110eab67
Andrzej <vim>
2018-08-07 14:39:05 +0200

    Works

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include<iostream>
 #include<algorithm>
 #include<vector>
-
+#include<map>
+#include<iterator>
+#include <stdexcept>
+#include <ctime>        // std::time
+#include <cstdlib>      // std::rand, std::srand
 
 int main()
 {
-    int n;
+    try
+    {
+    std::srand ( unsigned ( std::time(0) ) );
+    int n, m;
     std::cin >> n;
-    std::vector<int> prime(n-1);
+    std::cin >> m;
+    if (n <= 0 || n > m) throw std::invalid_argument("Bad data input");
+    std::vector<int> numbers(m+1);
+    std::iota(numbers.begin(), numbers.end(), 0);
+//take n random numbers form 0 to m
+    std::random_shuffle(numbers.begin(), numbers.end());
+    numbers.resize(n);
+    std::sort(numbers.begin(), numbers.end());
+//give prime numbers in range less than M
+    std::vector<int> prime(m-1);
     std::vector<int> p;
     std::iota(prime.begin(), prime.end(), 2);
     auto iterEnd = prime.end();
@@ -21,10 +37,42 @@ int main()
                     return 0;
                 });
     prime.resize(std::distance(prime.begin(), iterEnd));
-
-    for (auto iter : prime)
+//create map
+    std::map<int,std::vector<int>> map;
+    std::map<int,int> mapInt;
+    std::vector<int> h;
+    std::transform(prime.begin(), prime.end(),
+                prime.begin(),
+                std::inserter(map, map.begin()),
+                [& numbers, & map, & h](int p1, int p2) {
+                    h.clear();
+                    std::copy_if(numbers.begin(), numbers.end(),
+                             std::back_inserter(h),
+                             [& p1](int n) {
+                                return (n%p1==0 && n>=p1); 
+                             });
+                    return std::make_pair(p1,h);
+                });
+//show result
+    std::cout << "Random N numbers from M:" << std::endl;
+    for (auto iter : numbers)
     {
         std::cout << iter << std::endl;
+    }
+    std::cout << "Map:" << std::endl;
+    for (auto iter : map)
+    {
+        std::cout << iter.first << "  - > ";
+        for (auto iterV : iter.second) 
+        {
+            std::cout << " " << iterV; 
+        }
+        std::cout << std::endl;
+    }
+    }
+    catch(const std::invalid_argument & exc)
+    {
+        std::cout << exc.what() << std::endl;
     }
     return 0;
 }
